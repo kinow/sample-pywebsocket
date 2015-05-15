456704d4fbfacc189cb510a71bb992c62c506cce
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-05-16 00:02:16 +1200

    Update server and client

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
  #!/usr/bin/env python
 
+import sys
+
+from datetime import datetime
+
 from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory
+
+from twisted.internet import reactor
+from twisted.python import log
+
+from twisted.internet.task import LoopingCall
 
 class MyServer(WebSocketServerProtocol):
 
@@ -9,26 +18,27 @@ class MyServer(WebSocketServerProtocol):
 
     def onOpen(self):
         print("WebSocket connection open.")
+        lc = LoopingCall(self.sendTime)
+    	lc.start(5)
 
     def onMessage(self, payload, isBinary):
-    	"""You can treat text and binary messages here"""
-        pass
+    	#You can treat text and binary messages here
+    	if (isBinary):
+    		print "Binary message received!"
+    	else:
+        	print "Message received: %s" % payload
 
     def onClose(self, wasClean, code, reason):
         print("WebSocket connection closed: {0}".format(reason))
 
+    def sendTime(self):
+    	self.sendMessage(str(datetime.now()), isBinary=False)
 
 if __name__ == '__main__':
 
-    import sys
-
-    # This example uses Twisted for real-time web sockets.
-    from twisted.python import log
-    from twisted.internet import reactor
-
     log.startLogging(sys.stdout)
 
-    factory = WebSocketServerFactory("ws://.0.0.0.0:9000", debug=False)
+    factory = WebSocketServerFactory("ws://0.0.0.0:9000", debug=False)
     factory.protocol = MyServer
     # factory.setProtocolOptions(maxConnections=2)
 
